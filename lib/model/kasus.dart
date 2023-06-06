08350c88383f91b0a724fe6fdddcc2d46610502a
Muhammad Erald Setyaki Siregar <erald.muhammad@gmail.com>
2023-06-06 14:32:49 +0700

    add kasus

---
+++ lib/model/kasus.dart
@@ -0,0 +1,38 @@
+class Kasus {
+  String name = "";
+  List<String> listPertanyaan = [];
+
+  late List<bool> listJawaban;
+  late List<Map<String, dynamic>> rules;
+
+  Kasus(this.name, this.listPertanyaan) {
+    listJawaban = List<bool>.filled(listPertanyaan.length, false);
+  }
+
+  void addRules(Map<String, dynamic> rule) {
+    rules.add(rule);
+  }
+
+  List<String> getTags() {
+    for (int i = 0; i < rules.length; i++) {
+      if (isEqual(rules[i]["conditions"], listJawaban)) {
+        return rules[i]["tags"];
+      }
+    }
+    return [];
+  }
+
+  bool isEqual(List<bool> list1, List<bool> list2) {
+    if (list1.length != list2.length) {
+      return false;
+    }
+
+    for (int i = 0; i < list1.length; i++) {
+      if (list1[i] != list2[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
